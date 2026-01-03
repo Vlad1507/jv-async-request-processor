afc1f918ca1b1bfda1ef6ccd3ac2df42d4dc0f83
Vlad Andriichuk <vlad150096@gmail.com>
2026-01-03 11:57:55 +0100

    Implemented AsyncRequestProcessor class with method processorRequest() in context of asynchronous threads in multithreading.

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,38 @@
 package mate.academy;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private ConcurrentHashMap<String, CompletableFuture<UserData>> cache =
+            new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return cache.computeIfAbsent(userId,
+                key -> CompletableFuture
+                        .supplyAsync(() -> getUserData(key), executor)
+                        .whenComplete((result, throwable) -> {
+                            if (throwable != null) {
+                                cache.remove(key);
+                            }
+                        })
+        );
+    }
+
+    private static UserData getUserData(String key) {
+        try {
+            Thread.sleep(200);
+            return new UserData(key, "Details for " + key);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new CompletionException(e);
+        }
     }
 }
